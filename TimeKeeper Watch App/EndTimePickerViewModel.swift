d73d179e303c813c4ce8dc10a7e81c4b3c69d301
futaba3 <nyokinyoki41aya@gmail.com>
2024-08-16 21:17:53 +0900

    feat: EndTimePickerViewModelの中身を書く

--- TimeKeeper Watch App/EndTimePickerViewModel.swift
+++ TimeKeeper Watch App/EndTimePickerViewModel.swift
@@ -5,4 +5,15 @@
 //  Created by Ayana Kudo on 2024/08/16.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class EndTimePickerViewModel: ObservableObject {
+    @Published var selectedHour: Int = 0
+    @Published var selectedMinute: Int = 1
+    @Published var selectedSecond: Int = 0
+    
+    func totalSelectedTime() -> TimeInterval {
+        return TimeInterval(selectedHour * 3600 + selectedMinute * 60 + selectedSecond)
+    }
+}
