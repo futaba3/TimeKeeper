fa37b95f395f04adf269803ba2f16c3cae8327f1
futaba3 <nyokinyoki41aya@gmail.com>
2024-08-16 19:47:44 +0900

    Initial Commit

---
+++ TimeKeeper Watch App/TimeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeKeeperApp.swift
+//  TimeKeeper Watch App
+//
+//  Created by Ayana Kudo on 2024/08/16.
+//
+
+import SwiftUI
+
+@main
+struct TimeKeeper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
