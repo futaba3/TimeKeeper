1839af194e1de03ea00f9a6f1023ebde0c390885
futaba3 <nyokinyoki41aya@gmail.com>
2024-08-16 21:14:18 +0900

    feat: CountDownTimerViewModelの中身を書く

--- TimeKeeper Watch App/CountDownTimerViewModel.swift
+++ TimeKeeper Watch App/CountDownTimerViewModel.swift
@@ -5,4 +5,31 @@
 //  Created by Ayana Kudo on 2024/08/16.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class CountDownTimerViewModel: ObservableObject {
+    @Published var timeRemaining: TimeInterval = 0
+    private var timer: AnyCancellable?
+    
+    func startTimer(duration: TimeInterval) {
+        self.timeRemaining = duration
+        timer?.cancel()
+        
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self else { return }
+                if self.timeRemaining > 0 {
+                    self.timeRemaining -= 1
+                } else {
+                    self.timer?.cancel()
+
+                }
+            }
+    }
+    
+    func stopTimer() {
+        timer?.cancel()
+    }
+}
